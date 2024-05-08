f85b37161fbf89b0956fad14abf1338439a8012b
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-08 13:54:20 +0700

    :art: Eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['jest', '@typescript-eslint'],
+  rules: {
+    'no-useless-escape': 'off',
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'off',
+    'no-undef': 'off',
+    'prettier/prettier': ['error', {'no-inline-styles': false}],
+  },
 };
