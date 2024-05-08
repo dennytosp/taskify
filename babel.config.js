e6ccc9726b29b9fc29bf3c227fdd2839689ac3ad
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-08 14:05:35 +0700

    :wrench: Setup react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,25 +2,28 @@ const presets = ['module:@react-native/babel-preset'];
 
 const plugins = [];
 
-plugins.push([
-  'module-resolver',
-  {
-    root: ['./src'],
-    extensions: [
-      '.js',
-      '.jsx',
-      '.ts',
-      '.tsx',
-      '.android.js',
-      '.android.tsx',
-      '.ios.js',
-      '.ios.tsx',
-    ],
-    alias: {
-      '@': './src',
+plugins.push(
+  [
+    'module-resolver',
+    {
+      root: ['./src'],
+      extensions: [
+        '.js',
+        '.jsx',
+        '.ts',
+        '.tsx',
+        '.android.js',
+        '.android.tsx',
+        '.ios.js',
+        '.ios.tsx',
+      ],
+      alias: {
+        '@': './src',
+      },
     },
-  },
-]);
+  ],
+  'react-native-reanimated/plugin',
+);
 
 module.exports = {
   presets,
