0533e1aa639d3b4e04f2f9c6a5ba13bbd14a55d0
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-10 17:06:58 +0700

    :iphone: Responsive

--- src/utils/scale/index.ts
+++ src/utils/scale/index.ts
@@ -9,39 +9,34 @@ const [shortDimension, longDimension] =
 const guidelineBaseWidth = 393;
 const guidelineBaseHeight = 852;
 
- const scale = (size: number) =>
-  (shortDimension / guidelineBaseWidth) * size;
+const scale = (size: number) => (shortDimension / guidelineBaseWidth) * size;
 
- const verticalScale = (size: number) =>
+const verticalScale = (size: number) =>
   (longDimension / guidelineBaseHeight) * size;
 
- const moderateScale = (size: number, factor = 0.5) =>
+const moderateScale = (size: number, factor = 0.5) =>
   size + (scale(size) - size) * factor;
 
- const moderateVerticalScale = (size: number, factor = 0.5) =>
+const moderateVerticalScale = (size: number, factor = 0.5) =>
   size + (verticalScale(size) - size) * factor;
 
 const tiny = moderateScale(4); // 4
 const small = tiny * 2; // 8
 const mediumSmall = tiny * 3; // 12
 const regular = small * 2; // 16
+const icon = regular + tiny; // 20
 const large = small * 3; // 24
 const big = regular * 2; // 32
 const huge = large * 2; // 48
 
-
-export {
-  scale,
-  verticalScale,
-  moderateScale,
-  moderateVerticalScale,
-};
+export { scale, verticalScale, moderateScale, moderateVerticalScale };
 
 export const MetricsSizes = {
   tiny,
   small,
   mediumSmall,
   regular,
+  icon,
   large,
   big,
   huge,
