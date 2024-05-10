79f5331ea40d5584546bdb5cca69893f0efeb01e
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-10 17:21:40 +0700

    :bento: Update assets

---
+++ src/theme/assets.ts
@@ -0,0 +1,14 @@
+const Images = {};
+
+const Icons = {
+  bootsplashLogo: require('@/assets/icons/bootsplash_logo/bootsplash_logo.png'),
+  appIcon: require('@/assets/icons/app-icon/app-icon.png'),
+  logo: require('@/assets/icons/logo/logo.png'),
+  eyeOpen: require('@/assets/icons/eye-open/eye-open.png'),
+  eyeClose: require('@/assets/icons/eye-close/eye-close.png'),
+  facebook: require('@/assets/icons/facebook/facebook.png'),
+  google: require('@/assets/icons/google/google.png'),
+  forwardLeft: require('@/assets/icons/forward-left/forward-left.png'),
+};
+
+export { Icons, Images };
