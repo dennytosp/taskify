3ebae4f24a917145f5e21f33296d75b7a6fda7c0
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-10 17:24:18 +0700

    :sparkles: Authentication UI

--- src/screens/auth/ForgotPassword/index.tsx
+++ src/screens/auth/ForgotPassword/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { RoutesRootStack } from '@/navigators/routes';
+import { Button, Header, Input } from '@/components';
+import { InputRef } from '@/components/Input/type';
+import { RegularText } from '@/components/Text';
+import { RoutesAuthStack, RoutesRootStack } from '@/navigators/routes';
+import { AppStyles } from '@/styles';
+import { translate } from '@/translations/translate';
 import { styles } from './style';
 
 type NavigationProps =
@@ -10,9 +15,71 @@ type NavigationProps =
 const ForgotPassword = () => {
   const navigation = useNavigation<NavigationProps['navigation']>();
 
+  const emailRef = useRef<InputRef>(null);
+
+  const handleVerify = () => {
+    console.log('Pressed Verify');
+  };
+
+  const onNavigateSignIn = () => {
+    navigation.navigate(RoutesRootStack.AUTH_STACK, {
+      screen: RoutesAuthStack.SIGN_IN,
+    });
+  };
+
+  const renderHeader = () => (
+    <View style={[styles.wrapHeader]}>
+      <RegularText style={[styles.textContent]}>
+        {translate('taskify.auth.pleaseVerifyYourEmail')}
+      </RegularText>
+    </View>
+  );
+
+  const renderForm = () => (
+    <View style={[]}>
+      <Input
+        ref={emailRef}
+        isRequired={true}
+        placeHolder={'jonathan@example.com'}
+        inputProps={{ keyboardType: 'email-address', maxLength: 75 }}
+        title={translate('taskify.auth.emailAddress')}
+        containerStyle={[{ marginBottom: 0 }]}
+      />
+    </View>
+  );
+
+  const renderButton = () => (
+    <Button
+      title={translate('taskify.auth.verify')}
+      style={[styles.button]}
+      onPress={handleVerify}
+    />
+  );
+
+  const renderFooter = () => (
+    <View style={[AppStyles.rowCenter]}>
+      <RegularText style={[styles.textDidYouRemember]}>
+        {translate('taskify.auth.didYouRememberThePassword')}
+      </RegularText>
+      <TouchableOpacity onPress={onNavigateSignIn}>
+        <RegularText style={[styles.textSignIn]}>
+          {` ${translate('taskify.auth.signIn')}`}
+        </RegularText>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={[styles.container]}>
-      <Text>Forgot password</Text>
+      <Header title={translate('taskify.auth.forgotPassword')} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps={'handled'}>
+        {renderHeader()}
+        {renderForm()}
+        {renderButton()}
+        {renderFooter()}
+      </ScrollView>
     </View>
   );
 };
--- src/screens/auth/SignIn/index.tsx
+++ src/screens/auth/SignIn/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { RoutesRootStack } from '@/navigators/routes';
+import React, { useRef } from 'react';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import { Button, Image, Input } from '@/components';
+import { InputRef } from '@/components/Input/type';
+import { BoldText, RegularText, SemiBoldText } from '@/components/Text';
+import { RoutesAuthStack, RoutesRootStack } from '@/navigators/routes';
+import { AppStyles } from '@/styles';
+import { Icons } from '@/theme';
+import { translate } from '@/translations/translate';
+import { isIos } from '@/utils/device';
+import { moderateScale } from '@/utils/scale';
 import { styles } from './style';
 
 type NavigationProps =
@@ -9,10 +18,151 @@ type NavigationProps =
 
 const SignIn = () => {
   const navigation = useNavigation<NavigationProps['navigation']>();
+  const insets = useSafeAreaInsets();
+
+  const emailRef = useRef<InputRef>(null);
+  const passwordRef = useRef<InputRef>(null);
+
+  const handleSignIn = () => {
+    console.log('Pressed Sign In');
+  };
+
+  const handleGoogle = () => {
+    console.log('Pressed Google');
+  };
+
+  const handleFacebook = () => {
+    console.log('Pressed Facebook');
+  };
+
+  const onNavigateSignUp = () => {
+    navigation.navigate(RoutesRootStack.AUTH_STACK, {
+      screen: RoutesAuthStack.SIGN_UP,
+    });
+  };
+
+  const onNavigateForgotPassword = () => {
+    navigation.navigate(RoutesRootStack.AUTH_STACK, {
+      screen: RoutesAuthStack.FORGOT_PASSWORD,
+    });
+  };
+
+  const renderHeader = () => (
+    <View style={[styles.wrapHeader]}>
+      <Image source={Icons.logo} customStyle={[{ width: moderateScale(80) }]} />
+      <View style={[AppStyles.rowVCenter]}>
+        <RegularText style={[styles.textTitle]}>
+          {translate('taskify.auth.lets')}
+        </RegularText>
+
+        <BoldText style={[styles.textTitle]}>
+          {` ${translate('taskify.auth.signIn')}`}
+        </BoldText>
+      </View>
+
+      <RegularText style={[styles.textSubtitle]}>
+        {translate('taskify.auth.headerSubtitle')}
+      </RegularText>
+    </View>
+  );
+
+  const renderForm = () => (
+    <View style={[]}>
+      <Input
+        ref={emailRef}
+        isRequired={true}
+        placeHolder={translate('taskify.auth.enterEmail')}
+        inputProps={{ keyboardType: 'email-address', maxLength: 75 }}
+        title={translate('taskify.auth.emailAddress')}
+      />
+      <Input
+        ref={passwordRef}
+        isRequired={true}
+        placeHolder={translate('taskify.auth.enterPassword')}
+        inputProps={{ textContentType: 'oneTimeCode', maxLength: 50 }}
+        containerStyle={[{ marginBottom: 0 }]}
+        isPassword={true}
+        title={translate('taskify.auth.password')}
+      />
+    </View>
+  );
+
+  const renderButton = () => (
+    <Button
+      title={translate('taskify.auth.signIn')}
+      style={[styles.button]}
+      onPress={handleSignIn}
+    />
+  );
+
+  const renderForgotPassword = () => (
+    <TouchableOpacity
+      onPress={onNavigateForgotPassword}
+      style={[styles.wrapForgotPassword]}>
+      <SemiBoldText style={[styles.textForgotPassword]}>
+        {`${translate('taskify.auth.forgotPassword')}?`}
+      </SemiBoldText>
+    </TouchableOpacity>
+  );
+
+  const renderOrLine = () => (
+    <View style={[styles.wrapOr]}>
+      <View style={[styles.line]} />
+      <RegularText style={[styles.textOr]}>
+        {translate('taskify.auth.or')}
+      </RegularText>
+      <View style={[styles.line]} />
+    </View>
+  );
+
+  const renderSocialButton = () => (
+    <View style={[AppStyles.rowCenter, { marginBottom: moderateScale(40) }]}>
+      <TouchableOpacity
+        onPress={handleGoogle}
+        style={[styles.touchSocialButton]}>
+        <Image source={Icons.google} customStyle={[styles.iconSocial]} />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={handleFacebook}
+        style={[styles.touchSocialButton, { marginLeft: moderateScale(32) }]}>
+        <Image source={Icons.facebook} customStyle={[styles.iconSocial]} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderFooter = () => (
+    <View style={[AppStyles.rowCenter]}>
+      <RegularText style={[styles.textDontHaveAccount]}>
+        {translate('taskify.auth.dontHaveAnAccount')}
+      </RegularText>
+      <TouchableOpacity onPress={onNavigateSignUp}>
+        <RegularText style={[styles.textSignUp]}>
+          {` ${translate('taskify.auth.signUp')}`}
+        </RegularText>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <View style={[styles.container]}>
-      <Text>Sign in</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps={'handled'}
+        contentContainerStyle={[
+          {
+            paddingTop: isIos ? insets.top : moderateScale(32),
+            paddingBottom: insets.bottom,
+          },
+        ]}>
+        {renderHeader()}
+        {renderForm()}
+        {renderForgotPassword()}
+        {renderButton()}
+        {renderOrLine()}
+        {renderSocialButton()}
+        {renderFooter()}
+      </ScrollView>
     </View>
   );
 };
