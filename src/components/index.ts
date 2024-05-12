889c60ab9b18b982eb9e7c093238f8b0a82eb800
Phong Dinh <phong.dinh2108@gmail.com>
2024-05-12 23:33:54 +0700

    :sparkles: Check box component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
 import AnimatedBottomTabBar from './BottomTabBar';
 import Button from './Button';
+import { CheckBox } from './CheckBox';
 import Header from './Header';
 import Icon from './Icon';
 import { Image } from './Image';
 import Input from './Input';
 
-export { AnimatedBottomTabBar, Button, Header, Icon, Image, Input };
-
+export { AnimatedBottomTabBar, Button, CheckBox, Header, Icon, Image, Input };
